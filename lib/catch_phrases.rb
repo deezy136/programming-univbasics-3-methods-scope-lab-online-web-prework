a0e9e411751a83cfaaa7a4db10c5c9ee530d2f80
deezy13 <deezy13@gmail.com>
2019-09-05 17:02:21 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,11 @@
-$status = " "
+$status = "i"
+
 def mario
   $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
+
 
 def toadstool
   puts $status
